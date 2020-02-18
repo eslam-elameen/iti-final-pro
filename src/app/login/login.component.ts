045a7e9d9e73e44041315d03d43690a89902eed4
eslam-elameen <e.elameen92@gmail.com>
2020-02-18 16:16:19 +0200

    made the auth of login and signup and made them to navigate to profile component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { ApiService } from './../api.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  login: FormGroup;
+  checkDAta;
+  err;
+  constructor(private formBulider: FormBuilder, private validData: ApiService,private route:Router) { }
+
+  ngOnInit() {
+    this.login = this.formBulider.group({
+      email: ['', [Validators.required, Validators.pattern(/\w{1,}@[a-z]{3,}\.com/)]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-z0-9]{8,}[\W]{0,}$/)]]
+    })
+    this.validData.getUserData().subscribe(data => {
+      this.checkDAta = data
+    })
+  }
+
+
+  onLoginSubmit(form) {
+    if (form.valid) {
+      for (let i = 0; i < this.checkDAta.length; i++) {
+        if (form.value.email != this.checkDAta[i].email) {
+          document.getElementById('submitAlert').style.display = 'block';
+          this.route.navigate(['/profile']);
+        } else {
+          console.log('not valid email');
+          document.getElementById('submitAlert').style.display = 'none';
+        }
+      }
+    }
+  }
+}
