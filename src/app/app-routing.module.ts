e1b6474a9ca2034d41939a11981274bf8bcb3014
anasismail58 <anasismail58@gmail.com>
2020-02-21 19:15:16 +0200

    change router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path:"",component:LoginComponent},
+  {path:"",component:RegisterationComponent},
   {path:'profile',component:ProfileComponent},
   {path:'signin',component:LoginComponent}
 ];
