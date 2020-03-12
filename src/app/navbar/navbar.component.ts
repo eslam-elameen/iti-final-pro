5e266ab3ba3c2d9046b5df981c153cf27c54faf3
mrnobody87 <mamhoudgoweda87@gmail.com>
2020-03-12 19:44:35 +0200

    fix card bugs

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,12 @@
-import { ApiService } from './../api.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ProudctsService } from '../proudcts.service';
-import { FormControl } from '@angular/forms';
+import {Validators,FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-// import { CheckboxFilterService } from '../checkbox-filter.service';
 import { NgwWowService } from 'ngx-wow';
+import { ApiService } from './../api.service';
+import { ProudctsService } from '../proudcts.service';
 import { ShoppingCartService } from '../shopping-cart.service';
-import { CheckBoxFilterService } from '../check-box-filter.service';
-import { Router } from '@angular/router';
 
 export interface Product {
   category: string;
@@ -24,27 +21,32 @@ export interface Product {
 export class NavbarComponent implements OnInit {
   mySearch = new FormControl('',Validators.required);
   filterAutoComolete: Observable<Product[]>;
-  productsData;
-  filterd;
-  searchResult;
-  toggle
+  productsData:any[];
+  filterd:any[];
+  toggle;
   shoppingCartProduct;
   toggle3
   total;
-  // serviceTotal: number = 0;
-  // totalQty: number = 0;
   checkLogIn;
-  lox: string;
+  logCheck: string;
   logedin: any;
   userName: any;
-  comingDAta;
-  public constructor(private fb: FormBuilder,
-    private checkFilter: CheckBoxFilterService,
+  
+  private _filterStates(value: string): Product[] {
+    const filterValue = value.toLowerCase();
+    return this.productsData.filter(product =>  product.productTitle.toLowerCase().includes(filterValue));
+  }
+  
+  
+  public constructor(
     private searchServer: ProudctsService,
     private wowService: NgwWowService,
     private shoppingServices: ShoppingCartService,
-    private login: ApiService, private route: Router) {
-    this.searchServer.getData().subscribe(res => this.productsData = res)
+    private login: ApiService, 
+    private route: Router) 
+    {
+   
+      this.searchServer.getData().subscribe((res:any[]) => this.productsData = res)
     this.filterAutoComolete = this.mySearch.valueChanges
       .pipe(
         startWith(''),
@@ -54,14 +56,13 @@ export class NavbarComponent implements OnInit {
   }
   
   ngOnInit() {
-    this.lox = localStorage.getItem('logedin')
-    console.log(this.lox)
-
+    this.checkLogIn = localStorage.getItem('logedin')
+    console.log(this.checkLogIn)
     this.login.teat.subscribe(logCheck => {
       console.log(logCheck)
       logCheck ? localStorage.setItem('logedin', JSON.stringify(this.logedin))
         : console.log(logCheck)
-      this.lox = localStorage.getItem('logedin')
+      this.checkLogIn = localStorage.getItem('logedin')
     })
     this.userName = JSON.parse(localStorage.getItem('user'));
    
@@ -74,52 +75,30 @@ export class NavbarComponent implements OnInit {
     });
    
   }
-  private _filterStates(value: string): Product[] {
-    const filterValue = value.toLowerCase();
-    return this.productsData.filter(product =>  product.productTitle.toLowerCase().includes(filterValue));
-  }
   divToggle1(event) {
     this.toggle3 = !this.toggle3
   }
   droptoggle(event) {
     this.toggle = document.getElementById('navbarSupportedContent');
-    if (this.toggle.style.display === "none") {
-      this.toggle.style.display = "block";
-    }
-    else {
-      this.toggle.style.display = "none";
-    }
+    this.toggle.style.display === "none"?
+      this.toggle.style.display = "block":this.toggle.style.display = "none";
   }
   onSubmit(form) {
-    this.searchResult = (form.value)
-    console.log(this.searchResult)
-    this.searchServer.getResult(this.searchResult)
     console.log(form.value);
-
+    this.searchServer.getResult(form.value)
     this.mySearch.setValue('')
   }
   
 
   userLogout() {
     localStorage.clear();
-
     this.shoppingServices.products = [];
     console.log(this.shoppingServices.products);
-    this.lox = localStorage.getItem('logedin')
+    this.checkLogIn = localStorage.getItem('logedin')
     this.shoppingServices.getAllQuantityProduct()
-
     this.route.navigate(['/']);
-
   }
 }
 
-    // this.mySearch = this.fb.group({
-    //   search: ''
-    // });
-
-
-    // this.shoppingServices.sendCountQtyServices.subscribe(number => {
-    //   this.serviceTotal = number;
-    // })
 
 
